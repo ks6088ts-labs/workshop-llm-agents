cdbd1b830f00be6c5c18808b58c689f168034c5a
ks6088ts <ks6088ts@gmail.com>
2024-11-25 23:16:13 +0900

    Customizing State

--- scripts/langgraph_quickstart.py
+++ scripts/langgraph_quickstart.py
@@ -12,6 +12,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
+from pydantic import BaseModel
 from typing_extensions import TypedDict
 
 load_dotenv()
@@ -21,9 +22,20 @@ app = typer.Typer()
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+    # This flag is new
+    ask_human: bool
 
 
-graph_builder = StateGraph(State)
+class RequestAssistance(BaseModel):
+    """
+    Escalate the conversation to an expert.
+    Use this if you are unable to assist directly or if the user requires support beyond your permissions.
+    To use this function, relay the user's 'request' so the expert can provide the right guidance.
+    """
+
+    request: str
+
+
 tools = [
     BingSearchResults(
         api_wrapper=BingSearchAPIWrapper(k=2),
@@ -36,25 +48,57 @@ llm = AzureChatOpenAI(
     azure_endpoint=getenv("AZURE_OPENAI_ENDPOINT"),
     model=getenv("AZURE_OPENAI_GPT_MODEL"),
 )
-
-llm_with_tools = llm.bind_tools(tools)
+llm_with_tools = llm.bind_tools(tools + [RequestAssistance])
 
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    response = llm_with_tools.invoke(state["messages"])
+    ask_human = False
+    if response.tool_calls and response.tool_calls[0]["name"] == RequestAssistance.__name__:
+        ask_human = True
+    return {"messages": [response], "ask_human": ask_human}
 
 
+def create_response(response: str, ai_message: AIMessage):
+    return ToolMessage(
+        content=response,
+        tool_call_id=ai_message.tool_calls[0]["id"],
+    )
+
+
+def human_node(state: State):
+    new_messages = []
+    if not isinstance(state["messages"][-1], ToolMessage):
+        # Typically, the user will have updated the state during the interrupt.
+        # If they choose not to, we will include a placeholder ToolMessage to
+        # let the LLM continue.
+        new_messages.append(create_response("No response from human.", state["messages"][-1]))
+    return {
+        # Append the new messages
+        "messages": new_messages,
+        # Unset the flag
+        "ask_human": False,
+    }
+
+
+def select_next_node(state: State):
+    if state["ask_human"]:
+        return "human"
+    # Otherwise, we can route as before
+    return tools_condition(state)
+
+
+graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
-
-tool_node = ToolNode(tools=tools)
-graph_builder.add_node("tools", tool_node)
-
+graph_builder.add_node("tools", ToolNode(tools=tools))
+graph_builder.add_node("human", human_node)
 graph_builder.add_conditional_edges(
     "chatbot",
-    tools_condition,
+    select_next_node,
+    {"human": "human", "tools": "tools", "__end__": "__end__"},
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge("human", "chatbot")
 graph_builder.set_entry_point("chatbot")
 
 
@@ -64,7 +108,7 @@ def create_graph(
 ):
     return graph_builder.compile(
         checkpointer=MemorySaver() if memory else None,
-        interrupt_before=["tools"] if interrupt else None,
+        interrupt_before=["human"] if interrupt else None,
     )
 
 
@@ -100,25 +144,17 @@ def run(
 
     # If interrupted, type your pseudo response as AIMessage
     if snapshot.next:
-        answer = input("Enter user input: ")
-        new_messages = [
-            # The LLM API expects some ToolMessage to match its tool call. We'll satisfy that here.
-            ToolMessage(content=answer, tool_call_id=existing_message.tool_calls[0]["id"]),
-            # And then directly "put words in the LLM's mouth" by populating its response.
-            AIMessage(content=answer),
-        ]
-        print(new_messages[-1].pretty_print())
-        graph.update_state(
-            # Which state to update
-            config,
-            # The updated values to provide.
-            # The messages in our `State` are "append-only", meaning this will be appended
-            # to the existing state. We will review how to update existing messages in the next section!
-            {"messages": new_messages},
-        )
+        ai_message = snapshot.values["messages"][-1]
+        human_response = input("Enter human response: ")
+        tool_message = create_response(human_response, ai_message)
+        graph.update_state(config, {"messages": [tool_message]})
+        print(graph.get_state(config).values["messages"])
 
-    print("\n\nLast 2 messages;")
-    print(graph.get_state(config).values["messages"][-2:])
+        # Resume the graph by invoking it with None as the inputs
+        events = graph.stream(None, config, stream_mode="values")
+        for event in events:
+            if "messages" in event:
+                event["messages"][-1].pretty_print()
 
 
 @app.command()
