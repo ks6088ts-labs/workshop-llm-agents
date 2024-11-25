c83eb488d11ce4921e54861d9b645c93c6f07648
ks6088ts <ks6088ts@gmail.com>
2024-11-25 17:33:17 +0900

    Enhancing the Chatbot with Tools

--- scripts/langgraph_quickstart.py
+++ scripts/langgraph_quickstart.py
@@ -4,10 +4,13 @@ from typing import Annotated
 
 import typer
 from dotenv import load_dotenv
+from langchain_community.tools.bing_search import BingSearchResults
+from langchain_community.utilities import BingSearchAPIWrapper
 from langchain_core.messages import HumanMessage
 from langchain_openai import AzureChatOpenAI
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 from typing_extensions import TypedDict
 
 load_dotenv()
@@ -20,6 +23,11 @@ class State(TypedDict):
 
 
 graph_builder = StateGraph(State)
+tools = [
+    BingSearchResults(
+        api_wrapper=BingSearchAPIWrapper(k=2),
+    ),
+]
 llm = AzureChatOpenAI(
     temperature=0,
     api_key=getenv("AZURE_OPENAI_API_KEY"),
@@ -28,17 +36,25 @@ llm = AzureChatOpenAI(
     model=getenv("AZURE_OPENAI_GPT_MODEL"),
 )
 
+llm_with_tools = llm.bind_tools(tools)
+
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
 graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.set_entry_point("chatbot")
-graph_builder.set_finish_point("chatbot")
 graph = graph_builder.compile()
 
 
